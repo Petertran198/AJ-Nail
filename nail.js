c3f1567f2d20b4831ac00e5f809ff9b34c5af287
petertran198 <petertran198@gmail.com>
2019-05-11 14:08:34 -0400

    Made autoplay false in index.html

--- nail.js
+++ nail.js
@@ -22,7 +22,7 @@ $(function () {
   $(".video").click(function () {
     var theModal = $(this).data("target"),
       videoSRC = $(this).attr("data-video"),
-      videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+      videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=0";
     $(theModal + ' iframe').attr('src', videoSRCauto);
     $(theModal + ' button.close').click(function () {
       $(theModal + ' iframe').attr('src', videoSRC);
